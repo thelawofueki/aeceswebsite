f72a35daf1b49a47003550223770bb59e3b948a1
Charles Mholen Siloy <siembies@gmail.com>
2013-04-19 14:18:17 +0800

    Add a StaticPages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Aeces::Application.routes.draw do
-  # The priority is based upon order of creation:
+  get "static_pages/home"
+
+  get "static_pages/help"
+
+   # The priority is based upon order of creation:
   # first created -> highest priority.
 
   # Sample of regular route:
